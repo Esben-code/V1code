ab1f355c381585d0ec3af9fe55b16688719d633d
Esben-code <Esben.aka.bob@gmail.com>
2021-05-31 11:22:21 +0200

    javascript basics

---
+++ V2/JavaScript Conditions/script.js
@@ -0,0 +1,57 @@
+var moreThen50 = 73;
+
+if (moreThen50 > 50) 
+{
+    console.log('Tallet er større end 50.');
+}
+
+var minAge = 23;
+
+if (minAge >= 18) 
+{
+    console.log(minAge + ' er større end eller lig med 18.');    
+}
+
+var firstName = null;
+
+if (firstName == null) 
+{
+    console.log('Du glemte at indtaste dit fornavn');
+}
+
+var gender = 'male';
+
+if (gender == 'male') 
+{
+    console.log('Personen er en mand');   
+} 
+else 
+{
+    console.log('Personen er en kvinde');   
+}
+
+var counter = 25;
+
+if (counter =! 50) 
+{
+    console.log('Tallet er godkendt');    
+}
+else 
+{
+    console.log('Tallet er 50');
+}
+
+var myAge = 28;
+
+if (myAge <= 18) 
+{
+  console.log('Du er et barn');   
+}
+else if (18 < myAge < 62) 
+{
+    console.log('Du er voksen');    
+}
+else 
+{
+    console.log('Du er pensionist');
+}
