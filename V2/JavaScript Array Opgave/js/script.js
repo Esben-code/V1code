d938ebe7c48d8ceeb87ca2d99af569e0c95db8b4
Esben-code <Esben.aka.bob@gmail.com>
2021-06-03 14:39:23 +0200

    jscode

---
+++ V2/JavaScript Array Opgave/js/script.js
@@ -0,0 +1,45 @@
+var jyskeBank =
+{
+    bankNavn: 'Jyske Bank',
+    bankAdr: 'Skattelyvej 1',
+    bankZip: '2222',
+    bankBy: 'Solsiden',
+    bankTlfNr: '+45 1234 1234',
+    bankMail: 'kontakt@jyskebank.nu',
+    bankSender: 'Claus Clausen',
+    bankSenderTitle: 'Administrerende direktør',
+    bankImg: 'img/logo_jb.png',
+    bankImgAltTxt: 'Jyske Bank Logo'
+
+}
+
+var nordea =
+{
+    bankNavn: 'Nordea',
+    bankAdr: 'Købtgade 6',
+    bankZip: '1548',
+    bankBy: 'Gåserød',
+    bankTlfNr: '+45 8484 4848',
+    bankMail: 'info@nordea.dk',
+    bankSender: 'Karl Smart',
+    bankSenderTitle: 'CCO',
+    bankImg: 'img/logo_nd.png',
+    bankImgAltTxt: 'Nordea Logo'
+
+}
+
+var danskeBank =
+{
+    bankNavn: 'Danske Bank',
+    bankAdr: 'Tæppetovet 7',
+    bankZip: '2000',
+    bankBy: 'Pjort',
+    bankTlfNr: '+45 7777 7778',
+    bankMail: 'indbakke@danskebank.her',
+    bankSender: 'Hodja Ali',
+    bankSenderTitle: 'Administrerende direktør',
+    bankImg: 'img/logo_db.png',
+    bankImgAltTxt: 'Danske Bank Logo'
+
+}
+
