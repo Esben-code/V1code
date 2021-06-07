d7b2d69d62b800776254794d4bcc6b18dc6e6efa
Esben-code <Esben.aka.bob@gmail.com>
2021-06-07 14:43:22 +0200

    afafafafafhgrsgeg

---
+++ V2/DOMmanipulation/js/script.js
@@ -0,0 +1,55 @@
+"use strict";
+
+let menu = document.getElementById('menu');
+
+console.log(menu);
+
+////////////
+
+let activeMenu = document.getElementsByClassName('active');
+
+console.log(activeMenu);
+
+////////////
+
+let allh = document.querySelectorAll('h1')
+
+allh.forEach(element => { console.log(element)
+    
+});
+
+////////////
+
+let allp = document.querySelectorAll('p')
+
+allp.forEach(element => { console.log(element)
+
+})
+
+////////////
+
+let allImg = document.querySelectorAll('img')
+
+allImg.forEach(element => { element.setAttribute("alt", "Billedet er underlagt copyright")
+})
+
+////////////
+
+////////////
+
+////////////
+
+////////////
+
+////////////
+
+////////////
+
+////////////
+
+////////////
+
+////////////
+
+////////////
+
