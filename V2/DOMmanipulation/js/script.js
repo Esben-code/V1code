f7d1faa7187277a82c3486954df8d9fc5e314bf7
Esben-code <Esben.aka.bob@gmail.com>
2021-06-08 11:03:47 +0200

    5555555

--- V2/DOMmanipulation/js/script.js
+++ V2/DOMmanipulation/js/script.js
@@ -3,43 +3,66 @@
 let menu = document.getElementById('menu');
 
 console.log(menu);
+console.log("////////////");
 
 ////////////
 
 let activeMenu = document.getElementsByClassName('active');
 
 console.log(activeMenu);
+console.log("////////////");
 
 ////////////
 
-let allh = document.querySelectorAll('h1')
+let allh = document.querySelectorAll('h1');
 
-allh.forEach(element => { console.log(element)
+allh.forEach(element => { console.log(element);
     
 });
 
+console.log("////////////");
+
 ////////////
 
-let allp = document.querySelectorAll('p')
+let allp = document.querySelectorAll('p');
 
-allp.forEach(element => { console.log(element)
+allp.forEach(element => { console.log(element);
 
 })
 
+console.log("////////////");
+
 ////////////
 
-let allImg = document.querySelectorAll('img')
+let allImg = document.querySelectorAll('img');
 
-allImg.forEach(element => { element.setAttribute("alt", "Billedet er underlagt copyright")
+allImg.forEach(element => { element.setAttribute("alt", "Billedet er underlagt copyright");
 })
 
-////////////
+console.log("////////////");
 
 ////////////
 
-////////////
+document.querySelector('h1').innerHTML = "Overskriften er udskifted";
+
+console.log("////////////");
+
 
 ////////////
+
+document.querySelectorAll('h1').forEach(x => { x.classList.add("title")});
+
+console.log("////////////");
+
+////////////
+
+document.querySelectorAll("li").forEach(x => { console.log(x)});
+
+console.log("////////////");
+
+////////////
+
+let playground = document.querySelector('ID#playground')
 
 ////////////
 
